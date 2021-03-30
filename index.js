600be5a3a44ee88d06877a33b1c1d2f99a2d6625
dgornjakovic <dgornjakovic87@gmail.com>
2021-03-30 14:18:53 +0200

    cors

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ require("dotenv").config();
 var express = require("express");
 var app = express();
 
+var cors = require('cors');
+app.use(cors());
+
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server running on port " + (process.env.PORT || 3000));
 });
