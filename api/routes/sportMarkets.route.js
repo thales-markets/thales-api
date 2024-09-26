b198f115ea819bde46bbfe40482839910c2144c2
makiBaraba <markovic.ofsec@gmail.com>
2024-09-26 11:10:26 +0200

    Promotions route in sport-markets API routes with cache mechanism

--- api/routes/sportMarkets.route.js
+++ api/routes/sportMarkets.route.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const lpRoutes = require("./sportMarkets/lp.route");
 const referralRoutes = require("./sportMarkets/referral.route");
+const promotionsRoutes = require("./sportMarkets/promotions.route");
 
 router.use("/liquidity-providing", lpRoutes);
 router.use("/referral", referralRoutes);
+router.use("/promotions", promotionsRoutes);    
 
 module.exports = router;
