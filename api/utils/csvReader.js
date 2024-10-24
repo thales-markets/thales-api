ccba9a65f2317117ad8639e4f8db30f19f1d7fa9
Gorshtak <sg31191@gmail.com>
2024-10-24 14:10:24 +0200

    added leaguesMap to cache

---
+++ api/utils/csvReader.js
@@ -0,0 +1,49 @@
+const axios = require("axios");
+const stream = require("stream");
+const csvParser = require("csv-parser");
+
+/**
+ * Fetches and parses CSV data from a given URL.
+ *
+ * @param {string} url - The URL of the CSV file to fetch.
+ * @returns {Promise<Array<Object>>} - A promise that resolves to an array of parsed CSV data objects.
+ * @throws {Error} - Throws an error if the CSV data could not be fetched or parsed.
+ */
+async function readCsvFromUrl(url) {
+  try {
+    const response = await axios.get(url);
+    const csvContent = response.data;
+
+    const csvStream = new stream.Readable();
+    csvStream.push(csvContent);
+    csvStream.push(null);
+
+    const data = await parseCsv(csvStream);
+    return data;
+  } catch (error) {
+    console.error(`Failed to read CSV from URL: ${url}`, error);
+    throw error;
+  }
+}
+
+/**
+ * Parses CSV data from a readable stream.
+ *
+ * @param {stream.Readable} csvStream - A readable stream containing CSV data.
+ * @returns {Promise<Array<Object>>} - A promise that resolves to an array of parsed CSV data objects.
+ * @throws {Error} - Throws an error if the CSV data could not be parsed.
+ */
+function parseCsv(csvStream) {
+  return new Promise((resolve, reject) => {
+    const results = [];
+    csvStream
+      .pipe(csvParser())
+      .on("data", (data) => results.push(data))
+      .on("end", () => resolve(results))
+      .on("error", (error) => reject(error));
+  });
+}
+
+module.exports = {
+  readCsvFromUrl,
+};
