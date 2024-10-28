4334b2a61bd449bb0478af8d3ed703182d9d0037
Sasa Lotina <lotinasa@gmail.com>
2024-10-28 10:13:11 +0100

    Fix refactor bug on tahlesAPI

--- thalesApi/index.js
+++ thalesApi/index.js
@@ -2,8 +2,6 @@ const { initServices } = require("./services/init");
 const markets = require("./source/markets");
 require("dotenv").config();
 
-markets.processMarkets();
-
 const app = async () => {
   await initServices();
 
