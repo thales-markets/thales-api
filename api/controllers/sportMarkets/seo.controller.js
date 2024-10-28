b63e7d12d9fa4a6118e05e5759a36f386ce864b4
makiBaraba <markovic.ofsec@gmail.com>
2024-10-28 15:27:33 +0100

    seo routes fix

--- api/controllers/sportMarkets/seo.controller.js
+++ api/controllers/sportMarkets/seo.controller.js
@@ -2,29 +2,29 @@ const axios = require("axios");
 const cache = require("../../services/cache");
 
 const getSEOArticles = async (req, res) => {
-  const branchName = req.query["branch-name"] || "main";
-  const resetCache = req.query["reset-cache"] == "true";
-  const cacheKey = `seo_articles_${branchName}`;
-  const seoArticlesUrl = `https://raw.githubusercontent.com/thales-markets/thales-sport-markets/${branchName}/src/assets/seo/index.json`;
+  try {
+    const branchName = req.query["branch-name"] || "main";
+    const resetCache = req.query["reset-cache"] == "true";
+    const cacheKey = `seo_articles_${branchName}`;
+    const seoArticlesUrl = `https://raw.githubusercontent.com/thales-markets/thales-sport-markets/${branchName}/src/assets/seo/index.json`;
 
-  if (resetCache) {
-    cache.del(cacheKey);
-  }
+    if (resetCache) {
+      cache.del(cacheKey);
+    }
 
-  const seoArticlesData = cache.get(cacheKey);
+    const seoArticlesData = cache.get(cacheKey);
 
-  if (!seoArticlesData) {
-    try {
+    if (!seoArticlesData) {
       const response = await axios.get(seoArticlesUrl);
       cache.set(cacheKey, response.data);
-      res.send(response.data);
-    } catch (error) {
-      console.log("Error fetching seo articles data", error);
-      return res.status(500).send("Error fetching seo articles data");
+      return res.send(response.data);
     }
-  }
 
-  res.send(seoArticlesData);
+    return res.send(seoArticlesData);
+  } catch (error) {
+    console.log("Error fetching seo articles data", error);
+    return res.status(500).send("Error fetching seo articles data");
+  }
 };
 
 module.exports = {
